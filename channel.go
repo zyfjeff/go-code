24189b193709fa20f9184ace572ffa249e3db9a0
tianqian.zyf <tianqian.zyf@tianqianzyfdeMacBook-Pro.local>
2016-11-18 23:59:11 +0800

    6.824 course

--- channel.go
+++ channel.go
@@ -1,20 +1,27 @@
 package main
-import "fmt"
 
-func Count(ch chan int) {
-    ch <- 1
-    fmt.Println("Counting")
+import (
+	"fmt"
+	"time"
+)
+
+func printCount(c chan int) {
+	num := 0
+	for num >= 0 {
+		num = <-c
+		fmt.Print(num, " ")
+	}
 }
 
 func main() {
-    chs := make([]chan int,10)
-    for i := 0; i < 10; i++ {
-        chs[i] = make(chan int)
-        go Count(chs[i])
-    }
+	c := make(chan int)
+	a := []int{8, 6, 7, 5, 3, 0, 9, -1}
 
-    for _,ch := range(chs) {
-        <-ch
-    }
+	go printCount(c)
+	for _, v := range a {
+		c <- v
+	}
 
+	time.Sleep(time.Millisecond * 1)
+	fmt.Println("End of main")
 }
